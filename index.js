505c63bf776dd3a2fd9bc5117aeb15ab6e68dbbb
Jason Rothgeb <jrothgeb@gmail.com>
2022-07-12 11:32:31 -0500

    commit tree c627aab1263578396c7f27607c0ef819a1f918a2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(array,name) {
+    const result = array.filter(driver =>driver.toLowerCase() === name.toLowerCase());
+    return result;
+}
+
+function fuzzyMatch(array,name) {
+    const result = array.filter(driver => driver.charAt(0) ===  name.charAt(0));
+    return result;
+}
+
+function matchName (driverObjects,testName) {
+    const result = driverObjects.filter(driver => driver.name === testName);
+    return result;
+}
+
